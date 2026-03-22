e0ac00c40afcb29f7313cf390a5dbc96482118dd
logan0741 <logan0218@naver.com>
2026-03-22 22:42:17 +0900

    feature: extend spam keywords

--- app/spam.py
+++ app/spam.py
@@ -5,7 +5,7 @@ def check_spam(text: str) -> str:
 	spam_keywords = [
 		"free", "win", "winner", "prize", "click",
 		"buy now", "urgent", "cash", "money", "offer", "deal",
-		"discount", "limited time", "bonus"
+		"discount", "limited time", "bonus", "discount", "limited time", "bonus"
 	]
 	hit = 0
 	for kw in spam_keywords:
